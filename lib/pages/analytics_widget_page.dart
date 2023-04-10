e637f9490d714e5246304168c5e332773fbc7f7e
Lucas Hünniger <lucas.huenniger02@gmail.com>
2023-04-10 17:39:41 +0200

    feat: add progress-bar

--- lib/pages/analytics_widget_page.dart
+++ lib/pages/analytics_widget_page.dart
@@ -1,3 +1,4 @@
+import 'package:deskify/widgets/generic/progress_bar.dart';
 import 'package:flutter/material.dart';
 
 class AnalyticsWidgetPage extends StatefulWidget {
@@ -10,6 +11,26 @@ class AnalyticsWidgetPage extends StatefulWidget {
 class _AnalyticsWidgetPageState extends State<AnalyticsWidgetPage> {
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const ProgressBar(
+            height: 20.0,
+            displayValue: 0.5,
+            displayColor: Colors.green,
+          ),
+          const SizedBox(height: 10.0),
+          Center(
+            child: ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text("Back"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/generic/progress_bar.dart
@@ -0,0 +1,57 @@
+import 'package:deskify/utils.dart';
+import 'package:flutter/material.dart';
+
+class ProgressBar extends StatefulWidget {
+  final double height;
+  final double displayValue;
+  final Color displayColor;
+
+  const ProgressBar({
+    required this.height,
+    required this.displayValue,
+    this.displayColor = Colors.white,
+    super.key,
+  });
+
+  @override
+  State<ProgressBar> createState() => _ProgressBarState();
+}
+
+class _ProgressBarState extends State<ProgressBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        _buildProgressBar(),
+        const SizedBox(height: 10.0),
+        Center(child: _buildSemanticsLabel()),
+      ],
+    );
+  }
+
+  Widget _buildProgressBar() {
+    return SizedBox(
+      height: widget.height,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: LinearProgressIndicator(
+          value: widget.displayValue,
+          valueColor: AlwaysStoppedAnimation<Color>(widget.displayColor),
+          backgroundColor: Colors.grey[300],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSemanticsLabel() {
+    return Text(
+      "${Utils.roundDouble(widget.displayValue * 100, 1)}% completed",
+      style: const TextStyle(
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
