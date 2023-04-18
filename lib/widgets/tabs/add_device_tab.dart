cb2032bcdab5682dffe9ef57a966fefdb7c587e4
Lucas Hünniger <lucas.huenniger02@gmail.com>
2023-04-18 11:30:30 +0200

    feat: add general-inputs

--- lib/widgets/tabs/add_device_tab.dart
+++ lib/widgets/tabs/add_device_tab.dart
@@ -1,3 +1,4 @@
+import 'package:deskify/widgets/generic/heading_widget.dart';
 import 'package:flutter/material.dart';
 
 class AddDeviceTab extends StatefulWidget {
@@ -10,8 +11,38 @@ class AddDeviceTab extends StatefulWidget {
 class _AddDeviceTabState extends State<AddDeviceTab> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("AddDeviceTab"),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Heading(title: "General"),
+        _buildGeneralInput(),
+        const SizedBox(height: 10.0),
+        const Heading(title: "Presets"),
+        _buildPresetInput(),
+      ],
+    );
+  }
+
+  Widget _buildGeneralInput() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: const [
+        TextField(decoration: InputDecoration(labelText: 'Device Name')),
+        TextField(decoration: InputDecoration(labelText: 'Default Heigt')),
+      ],
+    );
+  }
+
+  Widget _buildPresetInput() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: const [
+        TextField(decoration: InputDecoration(labelText: 'Preset Name')),
+        TextField(decoration: InputDecoration(labelText: 'Preset Height')),
+      ],
     );
   }
 }
