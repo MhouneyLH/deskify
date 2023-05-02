4d196b3f5ed1f865f758a35833b4a6bef00d48d1
Lucas Hünniger <lucas.huenniger02@gmail.com>
2023-05-02 20:09:26 +0200

    feat: add presentation-readme

--- lib/widgets/tabs/add_desk_tab.dart
+++ lib/widgets/tabs/add_desk_tab.dart
@@ -43,6 +43,7 @@ class _AddDeskTabState extends State<AddDeskTab> {
           const SizedBox(height: 10.0),
           const Heading(title: 'Presets'),
           _buildPresetInput(),
+          const SizedBox(height: 10.0),
           _buildAddDeskButton(),
         ],
       ),
