021ef54cc74a2aa4f2d9fed967208b2036d8d5f8
Lucas Hünniger <lucas.huenniger02@gmail.com>
2023-05-06 22:01:25 +0200

    feat: add firebase-basic-integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:deskify/provider/desk_provider.dart';
 import 'package:deskify/provider/profile_provider.dart';
 import 'package:deskify/provider/theme_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'pages/home_page.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MainApp());
 }
 
