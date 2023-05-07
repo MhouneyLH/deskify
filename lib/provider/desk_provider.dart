696ca35c86e97290b24b64d82438cfe712706829
Lucas Hünniger <lucas.huenniger02@gmail.com>
2023-05-07 17:10:58 +0200

    fix: postFrameCallback-problem, where the data was a bit too late updated and so the user could see the updates

--- lib/provider/desk_provider.dart
+++ lib/provider/desk_provider.dart
@@ -21,56 +21,80 @@ class DeskProvider with ChangeNotifier {
   }
 
   void setDesks(List<Desk> desks) =>
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        _desks = desks;
-        notifyListeners();
-      });
+      WidgetsBinding.instance.addPostFrameCallback(
+        (_) {
+          _desks = desks;
+          notifyListeners();
+        },
+      );
 
   void addDesk(Desk desk) => FirebaseApi.createDesk(desk);
   void removeDesk(Desk desk) => FirebaseApi.deleteDesk(desk);
 
-  void udpateDeskName(Desk desk, String name) {
-    desk.name = name;
-    FirebaseApi.updateDesk(desk);
-    notifyListeners();
-  }
+  void udpateDeskName(Desk desk, String name) =>
+      WidgetsBinding.instance.addPostFrameCallback(
+        (_) {
+          desk.name = name;
+          FirebaseApi.updateDesk(desk);
+          notifyListeners();
+        },
+      );
 
-  void udpateDeskHeight(Desk desk, double height) {
-    final double inboundHeight = Desk.getInboundHeight(height);
-    desk.height = Utils.roundDouble(inboundHeight, 1);
+  void udpateDeskHeight(Desk desk, double height) =>
+      WidgetsBinding.instance.addPostFrameCallback(
+        (_) {
+          final double inboundHeight = Desk.getInboundHeight(height);
+          desk.height = Utils.roundDouble(inboundHeight, 1);
 
-    FirebaseApi.updateDesk(desk);
-    notifyListeners();
-  }
+          FirebaseApi.updateDesk(desk);
+          notifyListeners();
+        },
+      );
 
-  void udpateDeskPresets(Desk desk, List<Preset> presets) {
-    desk.presets = presets;
-    FirebaseApi.updateDesk(desk);
-    notifyListeners();
-  }
+  void udpateDeskPresets(Desk desk, List<Preset> presets) =>
+      WidgetsBinding.instance.addPostFrameCallback(
+        (_) {
+          desk.presets = presets;
+
+          FirebaseApi.updateDesk(desk);
+          notifyListeners();
+        },
+      );
 
   /// PRESET ///
-  void addPreset(Desk desk, Preset preset) {
-    desk.presets.add(preset);
-    FirebaseApi.updateDesk(desk);
-    notifyListeners();
-  }
+  void addPreset(Desk desk, Preset preset) =>
+      WidgetsBinding.instance.addPostFrameCallback(
+        (_) {
+          desk.presets.add(preset);
 
-  void updatePresetTitle(Desk desk, Preset preset, String title) {
-    final int index = desk.presets.indexWhere(
-      (element) => element.id == preset.id,
-    );
+          FirebaseApi.updateDesk(desk);
+          notifyListeners();
+        },
+      );
 
-    preset.title = title;
-    desk.presets[index] = preset;
+  void updatePresetTitle(Desk desk, Preset preset, String title) =>
+      WidgetsBinding.instance.addPostFrameCallback(
+        (_) {
+          final int index = desk.presets.indexWhere(
+            (element) => element.id == preset.id,
+          );
 
-    FirebaseApi.updateDesk(desk);
-    notifyListeners();
-  }
+          preset.title = title;
+          desk.presets[index] = preset;
 
-  void updatePresetTargetHeight(Desk desk, Preset preset, double targetHeight) {
-    preset.targetHeight = Utils.roundDouble(targetHeight, 1);
-    FirebaseApi.updateDesk(desk);
-    notifyListeners();
-  }
+          FirebaseApi.updateDesk(desk);
+          notifyListeners();
+        },
+      );
+
+  void updatePresetTargetHeight(
+          Desk desk, Preset preset, double targetHeight) =>
+      WidgetsBinding.instance.addPostFrameCallback(
+        (_) {
+          preset.targetHeight = Utils.roundDouble(targetHeight, 1);
+
+          FirebaseApi.updateDesk(desk);
+          notifyListeners();
+        },
+      );
 }
